864f451d387c93324987b89df4231e1e0a137119
Sajin <sajinstar2001@gmail.com>
2021-07-18 12:45:19 +0530

    added anime

--- app.js
+++ app.js
@@ -21,24 +21,110 @@ var swiper = new Swiper(".mySwiper3", {
   },
 });
 
+
+
   //  gsap anime
 
+
+const frameImg=document.querySelector(".landingPageImageInside")
+
   swiper1.on("transitionEnd", function () {
+   
     if (isOdd(swiper1.realIndex)) {
-      gsap.to(".landingRotatingSvg", { left: 0, rotate: 180, top: 0 });
+      gsap.to(".landingRotatingSvg", { left: 0, rotate: 180, top: 0 , duration:2,  });
     } else {
       gsap.to(".landingRotatingSvg", {
         left: 0,
-        rotate: 0,
+        rotate:0,
         bottom: -100,
         top: "initial",
+        duration:1.5,
+
       });
     }
+    if (swiper1.realIndex==0){
+      gsap.from(".landingSectionApp", { 
+        scrollTrigger:"landingSectionApp",
+        opacity: .2,
+         duration:2, 
+         y:-200 ,yoyo:true,
+      });
+      gsap.from(".landingPageImageBgText", { 
+        opacity: .2,
+         duration:2, 
+         y:-200 
+      });
+      gsap.from(".landingPageBotText", {
+         opacity: .2, 
+         duration:2, 
+         x:-300 
+        });
+      gsap.from(".landingPageStore", {
+         opacity: .2,
+          duration:2,
+           y:300 
+        });
+       frameImg.src="./Img/landingpageImg/1 page image.png"
+       
+   
+    }
+    if (swiper1.realIndex==1){
+      gsap.from(".secondSliderIcon", {
+        opacity: 1,
+         duration:3,
+          y:100 ,
+       });
+       gsap.from(".landingPageImageBgText", { 
+        opacity: .2,
+         duration:2, 
+         y:-200 
+      });
+       frameImg.src="./Img/secondSlider/2nd page image.png"
+       
+    }else if (swiper1.realIndex==2){
+      gsap.from(".thirdSliderIcons",{
+        opacity:0.2,
+        duration:2,
+        x:50,
+        y:50,
+        stagger:0.2,
+      })
+      gsap.from(".landingPageImageBgText", { 
+        opacity: .2,
+         duration:2, 
+         y:-200 
+      });
+      frameImg.src="./Img/thirdSlider/thirdsliderMblImg.png"
+    }
+    
   });
-  
+
+
+
   function isOdd(num) {
     return num % 2;
   }
 
-gsap.from(".secondSliderIcon", { opacity: 1, duration: 3, x: -500 });
 
+gsap.from(".secondSliderIcon", {
+  // scrollTrigger:".secondSliderIcon",
+   opacity: 1,
+    duration:3,
+     y:100 ,
+  });
+
+//   const myImgs = document.querySelectorAll('.animate-me');
+
+// let observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.intersectionRatio > 0) {
+//       console.log('in the view');
+//     } else {
+//       console.log('out of view');
+//     }
+//   });
+// });
+
+// myImgs.forEach(image => {
+//   observer.observe(image);
+// });
