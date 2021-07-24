29b1af9ffdb1e1828fcc723415313a60c20f3cc7
SajinStar <69190568+SajinStar@users.noreply.github.com>
2021-07-24 21:08:14 +0530

    fixed testimonial slider

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var swiper1 = new Swiper(".mySwiper1", {
   pagination: {
     el: ".swiper-pagination",
-    clickable: true, 
+    clickable: true 
   },
   direction: "vertical",
   mousewheel: {
@@ -47,6 +47,45 @@ var swiper = new Swiper(".mySwiper3", {
     },
   },
 
+});
+
+var swiper = new Swiper(".mySwiper4", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: false,
+  mousewheelControl: true,
+  loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    450: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
+  
 });
 
 
